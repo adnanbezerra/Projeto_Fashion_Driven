684031aa27fe1ebaba87d6c75099e72a402e4f38
adnan <adnanbezerra@gmail.com>
2022-05-04 22:49:40 -0300

    montar blusa

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,17 +1,26 @@
 // const NAME = prompt("E qual a sua graça?");
+const SHIRT_TYPE = ['t-shirt', 'top-tank', 'long'];
+const COLLAR_TYPE = ['v-neck', 'round', 'polo'];
+const TISSUE_TYPE = ['polyester', 'silk', 'cotton'];
+const recentOrders = []
+const shirt = {
+    model: "",
+    neck: "",
+    material: "",
+    owner: "NAME",
+    image: "",
+}
 
 function getDataFromAPI() {
     const promiseAPI = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
-    const recentOrders = []
 
     promiseAPI.then( (answer) => { 
         for(let i = 0; i < answer.data.length; i++) recentOrders.push(answer.data[i]);
-        console.log(answer);
-        addDataOnTheScreen(recentOrders);
+        addDataOnTheScreen();
     });
 }
 
-function addDataOnTheScreen(recentOrders) {
+function addDataOnTheScreen() {
     const apiRow = document.querySelector(".api-row");
 
     for(let i = 0; i < recentOrders.length; i++) {
@@ -23,8 +32,18 @@ function addDataOnTheScreen(recentOrders) {
     }
 }
 
-function selectItem(type) {
-    const type = type;
+function selectItem(type, order) {
+    let lastSelect = document.querySelector(`.${type}.selected`);
+    if(lastSelect) {
+        lastSelect.classList.toggle("selected");
+    }
+    console.log(lastSelect);
+
+    order.classList.toggle("selected")
+
+    if(type==='tissue') shirt.material = order.innerText;
+    if(type==='collar') shirt.neck = order.innerText;
+    if(type==='shirt') shirt.model = order.innerHTML;
 }
 
 getDataFromAPI();
